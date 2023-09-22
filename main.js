c1eba98d1e197c8c5cc1d009e194b8b7de437e45
Ancreem <ancreemroyal@gmail.com>
2023-09-21 23:21:45 -0500

    feat: :sparkles: Se adjunto API.
    se adjunto el link de la  url que se utilizara en el codigo.
    
    @Ancreem

---
+++ main.js
@@ -0,0 +1,8 @@
+const formulario = document.querySelector("#formAdd");
+const formEdit = document.querySelector("#formEdit");
+const dialog = document.querySelector("dialog");
+
+console.log(dialog)
+
+const url = "https://650cfbe547af3fd22f6817b0.mockapi.io/table"
+
