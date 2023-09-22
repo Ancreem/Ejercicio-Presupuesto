f78fc42dc0c4b6fc2576aa2db0e54d3126f74ed3
Ancreem <ancreemroyal@gmail.com>
2023-09-21 23:36:16 -0500

    feat: :sparkles: Creacion del metodo de escucha para enviar los datos.
    Se hizo un metodo de escucha para enviar la informacion dada hacia la api

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ console.log(dialog)
 
 const url = "https://650cfbe547af3fd22f6817b0.mockapi.io/table"
 
+formulario.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+    
+    let dato = Object.fromEntries(new FormData(e.target));
+
+    const res = await fetch(url);         
+    console.log(res);
+
+    const data = await res.json();
+    console.log(data);
+
+    let config = {
+        // metodo de 
+        method: "POST",
+        headers:{"content-type":"application/json"},
+        // Pasar a JSON el dato
+        body:JSON.stringify(dato)
+    }
+    const envio = await (await fetch(url,config)).json();
+    location.reload()
+});
