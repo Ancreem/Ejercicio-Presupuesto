68e8b769d544dc3a57feee8f27b3312a956b5b76
Ancreem <ancreemroyal@gmail.com>
2023-09-25 07:42:54 -0500

    feat: :sparkles: Div nuevos y ingreso a pokemones
    Se ingreso al contenido de cada pokemon
    
    Se agregaron mas div para organizar mejor el codigo y editarlo en css

--- main.js
+++ main.js
@@ -2,34 +2,51 @@ let bodyPag = document.querySelector("body");
 
 
 // Realizar una solicitud a la API de Pokémon
-fetch('https://pokeapi.co/api/v2/pokemon/')
+fetch(`https://pokeapi.co/api/v2/pokemon/`)
   .then((res)=> res.json())
-  .then(data => {
+  .then(data => crearPoke(data))
+
+function apiCont(namePoke){
+  fetch(`https://pokeapi.co/api/v2/pokemon/${namePoke}`)
+  .then((res)=> res.json())
+  .then(data => console.log(data))
+}
+
+
+  function crearPoke(data){
     
-
+    const tituloCont = document.createElement("div")
+    const titulo = document.createElement("h1")
+    const container = document.createElement("div")
     const carta = document.createElement(`div`)
-    carta.classList.add("Pokemones")
+    tituloCont.classList.add("tituloCont")
+    titulo.innerText = "Pokedex"
+    carta.classList.add("pokemones")
+    container.classList.add("container")
 
-
+    
+    
     // Recorrer los resultados
     data.results.forEach(pokemon => {
       const botPoke = document.createElement(`button`)
+      const nombrePoke = pokemon.name
       botPoke.id = "myPokemon"
       botPoke.innerText = pokemon.name
+      apiCont(nombrePoke)
+
       botPoke.addEventListener(`click`, () => {
         Swal.fire({
             title: `${pokemon.name}`,
-            text: 'Modal with a custom image.',
-            
+
         });
       })
 
+      tituloCont.appendChild(titulo)
       carta.appendChild(botPoke)
+      container.appendChild(tituloCont)
+      container.appendChild(carta)
       
-      bodyPag.appendChild(carta)
+      bodyPag.appendChild(container)
     });
-  })
-  .catch(error => {
-    console.error('Error al obtener datos de la API de Pokémon:', error);
-  });
+  }
 
