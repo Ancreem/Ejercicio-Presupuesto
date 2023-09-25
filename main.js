f09904e09deab2e67b46cee3abe8ee84d161c0d1
Ancreem <ancreemroyal@gmail.com>
2023-09-24 23:57:39 -0500

    feat: :sparkles: Codigo botones.
    Se creo el codigo de los botones donde iran los pokemones.
    
    Falta aprender a como recorrer cada elemento de cada pokemon.
    
    Se agrego el listener para cuando se le da click al boton que muestre el nombre del pokemon en una carta

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+let bodyPag = document.querySelector("body");
+
+
+// Realizar una solicitud a la API de Pokémon
+fetch('https://pokeapi.co/api/v2/pokemon/')
+  .then((res)=> res.json())
+  .then(data => {
+    
+
+    const carta = document.createElement(`div`)
+    carta.classList.add("Pokemones")
+
+
+    // Recorrer los resultados
+    data.results.forEach(pokemon => {
+      const botPoke = document.createElement(`button`)
+      botPoke.id = "myPokemon"
+      botPoke.innerText = pokemon.name
+      botPoke.addEventListener(`click`, () => {
+        Swal.fire({
+            title: `${pokemon.name}`,
+            text: 'Modal with a custom image.',
+            
+        });
+      })
+
+      carta.appendChild(botPoke)
+      
+      bodyPag.appendChild(carta)
+    });
+  })
+  .catch(error => {
+    console.error('Error al obtener datos de la API de Pokémon:', error);
+  });
 
